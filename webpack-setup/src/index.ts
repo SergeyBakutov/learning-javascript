68d84ac828a9691e0e2c0295a0058a1ea1e27d0d
Sergey Bakutov <sdb98@mail.ru>
2021-10-04 19:02:31 +0300

    webpack setup without eslint

---
+++ webpack-setup/src/index.ts
@@ -0,0 +1,21 @@
+class Person {
+  name = 'Sergey Bakutov'
+  age = 23
+}
+
+const person = new Person()
+
+const p = document.createElement('p')
+p.textContent = person.name
+const root = document.querySelector('#root')
+root.append(p)
+
+const x: number = 32
+console.log(x)
+
+const sum = (a: number, b: number): number => {
+  return a + b
+}
+
+console.log(sum(1, 41))
+console.log('Возраст:', person.age)
